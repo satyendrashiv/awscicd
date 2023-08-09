d168339fd8546d56ece8da96920d563f444b0af7
satyendrashiv <satyendrashiv@gmail.com>
2023-08-09 23:42:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,6 @@ provider "aws" {
   secret_key = "" 
   region     = "ap-south-1"
 }
-
- terraform {   
-backend "s3" {
-    bucket = "tf-state-s3"
-    key    = "terraform.tfstate"
-    region = "us-east-1" 
-  }
-}
-
 resource "aws_instance" "web_tf" {
   ami           = "ami-0ded8326293d3201b"
   instance_type = "t2.micro"
